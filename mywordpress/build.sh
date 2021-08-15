029f0fbe8e50e02d50dec8ab3421be3250de96a1
Sheraz <sheraz@mbp.com>
2021-08-15 11:31:18 -0400

    wordpress

--- mywordpress/build.sh
+++ mywordpress/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Temporary Remove commands. Remove them
 sudo rm -rf data
+docker rm -f $(docker ps -a -q)
+
+
 # Var Names
 project_name="test_wordpress"
 project_network="${project_name}_network"
@@ -20,30 +24,39 @@ then
     echo "Stopping..."
     docker stop $project_name
     docker stop $project_db
-    docker stop $project_name
 
     # Clean
     echo "Cleaning..."
     docker network rm $project_network
     docker rm $project_db
+    docker rm $project_name
 
     # Build
     echo "Building..."
-    docker network create --driver=bridge $project_network
+    # docker network create --driver=bridge $project_network
+
+echo "${password}"
 
     docker run --detach \
         -p 3306:3306 \
         -p 33060:33060 \
         --name $project_db \
-        --env="MYSQL_ROOT_PASSWORD=password123" \
-        --env="MYSQL_DATABASE=$project_db" \
-        --env="MYSQL_USER=$project_db" \
-        --env="MYSQL_PASSWORD=password123" \
+        --env="MYSQL_ROOT_PASSWORD=${password}" \
+        --env="MYSQL_DATABASE=${project_db}" \
+        --env="MYSQL_USER=${project_db}" \
+        --env="MYSQL_PASSWORD=${password}" \
         -v $path_db:/var/lib/mysql \
         --network $project_network \
-        -d mysql
+        mysql
 
-        
+    # docker run --detach \
+    #     -p 8080:80 \
+    #     --name $project_name \
+    #     --env="WORDPRESS_DB_HOST=${project_db}:3306" \
+    #     --env="WORDPRESS_DB_USER=${project_db}" \
+    #     --env="WORDPRESS_DB_PASSWORD=${password}" \
+    #     --network $project_network \
+    #     wordpress
 
 elif [ "$1" == "restart" ];
 then
