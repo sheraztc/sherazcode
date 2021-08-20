44f670215f0f22417fd657ad90d22866b1b3e44d
Sheraz <sheraz@mbp.com>
2021-08-20 07:14:09 -0400

    Upgrade CDB

---
+++ upgrade_cdb_version/upgrade_cdb_version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O /dev/null -o /dev/null https://masjiddashboard.com/api/version/company/5e8a32520680760b70035bbf/data/upgrade
+wget -O /dev/null -o /dev/null https://masjiddashboard.com/api/version/company/5f64d281cf4abc46de6b52ae/data/upgrade
+wget -O /dev/null -o /dev/null https://masjiddashboard.com/api/version/company/601178cedc813a7769981d34/data/upgrade
+wget -O /dev/null -o /dev/null https://masjiddashboard.com/api/version/company/6046bc4b37e1105575a61ef0/data/upgrade
+wget -O /dev/null -o /dev/null https://masjiddashboard.com/api/version/company/6075f30eaccfbc13622dbc26/data/upgrade
+wget -O /dev/null -o /dev/null https://masjiddashboard.com/api/version/company/60984534accfbc13622dbc35/data/upgrade
